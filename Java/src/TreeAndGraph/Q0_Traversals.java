f7a5dd0711ff67c24e0e42a843f2bf6fee8126e8
Angshu <angshu.gupta789@gmail.com>
2017-06-26 09:43:51 +0530

    Day22

--- Java/src/TreeAndGraph/Q0_Traversals.java
+++ Java/src/TreeAndGraph/Q0_Traversals.java
@@ -1,5 +1,37 @@
 package TreeAndGraph;
 
-public class Q0_Traversals {
+import ds.TreeNode;
 
+public class Q0_Traversals {
+	public static void visit(TreeNode node){
+		if(node!=null) System.out.println(node.data);
+	}
+	public static void inOrderTraversal(TreeNode node){
+		if(node!=null){
+			inOrderTraversal(node.left);
+			visit(node);
+			inOrderTraversal(node.right);
+		}
+	}
+	public static void preOrderTraversal(TreeNode node){
+		if(node!=null){
+			visit(node);
+			preOrderTraversal(node.left);
+			preOrderTraversal(node.right);
+		}
+	}
+	public static void postOrderTraversal(TreeNode node){
+		if(node!=null){
+			postOrderTraversal(node.left);
+			postOrderTraversal(node.right);
+			visit(node);
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr = {1,4,3,2,7,5,6};
+		TreeNode root = TreeNode.createMinimalBST(arr);
+		root.print();
+		postOrderTraversal(root);
+		
+	}
 }
